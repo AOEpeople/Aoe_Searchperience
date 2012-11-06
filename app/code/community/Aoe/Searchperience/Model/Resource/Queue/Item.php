c89f8a3073104ccf82b980fabbd6e082ea8e4e6e
Tymoteusz Motylewski <t.motylewski@gmail.com>
2012-11-06 15:33:59 +0100

    Create table for queue

--- app/code/community/Aoe/Searchperience/Model/Resource/Queue/Item.php
+++ app/code/community/Aoe/Searchperience/Model/Resource/Queue/Item.php
@@ -3,7 +3,7 @@
 class Aoe_Searchperience_Model_Resource_Queue_Item extends Mage_Core_Model_Resource_Db_Abstract {
 
 	public function _construct() {
-		$this->_init('aoesearchperience/queue_item', 'id');
+		$this->_init('aoe_searchperience/queue_item', 'queue_item_id');
 	}
 
 }
