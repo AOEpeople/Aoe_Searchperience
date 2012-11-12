202595c02bba3d7ee8ff71e4682ce9f3e8fea839
Tymoteusz Motylewski <t.motylewski@gmail.com>
2012-11-12 12:30:44 +0100

    cleanup, remove dependency for aoe_queue

--- app/code/community/Aoe/Searchperience/Model/Resource/Queue/Item/Collection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Aoe_Searchperience_Model_Resource_Queue_Item_Collection extends Mage_Core_Model_Resource_Db_Collection_Abstract {
-
-	public function _construct() {
-		$this->_init('aoe_searchperience/queue_item');
-	}
-
-}
