9f2a4119f1c8a117a6660da196140ad95d7a4670
Daniel Zohm <daniel.zohm@aoemedia.de>
2012-11-23 11:02:52 +0100

    added mime type for document

--- app/code/community/Aoe/Searchperience/Model/Client/Searchperience.php
+++ app/code/community/Aoe/Searchperience/Model/Client/Searchperience.php
@@ -153,6 +153,7 @@ class Aoe_Searchperience_Model_Client_Searchperience extends Apache_Solr_Service
             $document->setContent($this->_documentToXmlFragment($rawDocument));
             $document->setForeignId($this->_getValueFromArray('unique', $productData));
             $document->setSource($this->_documentSource);
+            $document->setMimeType('application/xml');
             $document->setUrl($this->_getValueFromArray('url', $productData));
 
             try {
