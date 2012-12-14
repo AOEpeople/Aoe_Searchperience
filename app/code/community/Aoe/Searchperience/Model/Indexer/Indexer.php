7a4dc2d538ce06f8073b0ed8f5c6698c8943635f
Tymoteusz Motylewski <t.motylewski@gmail.com>
2012-12-14 11:45:41 +0100

    Make searchperience_eventprocessing scheduler job process queue only
    This cron job will only process events from queue, even if process
    is in require_reindex state.

--- app/code/community/Aoe/Searchperience/Model/Indexer/Indexer.php
+++ app/code/community/Aoe/Searchperience/Model/Indexer/Indexer.php
@@ -10,8 +10,10 @@ class Aoe_Searchperience_Model_Indexer_Indexer
     public function reindexAll()
     {
         /* Change index status to running */
+        /* @var $indexProcess Mage_Index_Model_Process */
         $indexProcess = Mage::getSingleton('index/indexer')->getProcessByCode('catalogsearch_fulltext');
         if ($indexProcess) {
+            $indexProcess->setForcePartialReindex(TRUE);
             $indexProcess->reindexAll();
         }
 
