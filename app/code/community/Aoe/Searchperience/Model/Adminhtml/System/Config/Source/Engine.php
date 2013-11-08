975e072593c2624573f27147a3ef8a3b064ee2dd
Fabrizio Branca <fabrizio.branca@aoemedia.de>
2013-11-07 21:44:27 -0800

    Refactored config source model

--- app/code/community/Aoe/Searchperience/Model/Adminhtml/System/Config/Source/Engine.php
+++ app/code/community/Aoe/Searchperience/Model/Adminhtml/System/Config/Source/Engine.php
@@ -6,30 +6,18 @@
  * @package     Aoe_Searchperience
  * @author      Christoph Frenes <christoph.frenes@aoemedia.de>
  */
-class Aoe_Searchperience_Model_Adminhtml_System_Config_Source_Engine
+class Aoe_Searchperience_Model_Adminhtml_System_Config_Source_Engine extends Enterprise_Search_Model_Adminhtml_System_Config_Source_Engine
 {
     /**
-     * @see Enterprise_Search_Model_Adminhtml_System_Config_Source_Engine
      * @return array options for select field
      */
     public function toOptionArray()
     {
-        $searchperienceHelper = Mage::helper('aoe_searchperience'); /* @var $searchperienceHelper Aoe_Searchperience_Helper_Data */
-
-        $engines = array(
-            'catalogsearch/fulltext_engine'      => Mage::helper('enterprise_search')->__('MySql Fulltext'),
-            'aoe_searchperience/engine' => $searchperienceHelper->__('Searchperience')
+        $options = parent::toOptionArray();
+        $options[] = array(
+            'value' => 'aoe_searchperience/engine',
+            'label' => Mage::helper('aoe_searchperience')->__('Searchperience')
         );
-
-        if ($searchperienceHelper->isEnterprise()) $engines['enterprise_search/engine'] = Mage::helper('enterprise_search')->__('Solr');
-
-        $options = array();
-        foreach ($engines as $k => $v) {
-            $options[] = array(
-                'value' => $k,
-                'label' => $v
-            );
-        }
         return $options;
     }
 }
