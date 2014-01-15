593e2c6b28f5ec241a69980c97eb28f6e0c8fd2e
Fabrizio Branca <fabrizio.branca@aoemedia.de>
2014-01-15 14:57:19 -0800

    Added configuration option to disable certain data to be generated

--- app/code/community/Aoe/Searchperience/Model/Adapter/Searchperience.php
+++ app/code/community/Aoe/Searchperience/Model/Adapter/Searchperience.php
@@ -192,9 +192,6 @@ class Aoe_Searchperience_Model_Adapter_Searchperience extends Enterprise_Search_
             $product = Mage::getModel('catalog/product')->setStoreId($storeId)->load($productId);
         }
 
-        // fetch review data for requested product
-        Mage::getModel('review/review')->getEntitySummary($product, $storeId);
-
         // Set store as product store to avoid url param additions
         $currentStore = Mage::app()->getStore();
         Mage::app()->setCurrentStore($storeId);
@@ -203,31 +200,48 @@ class Aoe_Searchperience_Model_Adapter_Searchperience extends Enterprise_Search_
         $returnData['productData']['sku']    = $productIndexData['sku'];
         $returnData['productData']['url']    = $product->getProductUrl();
         $returnData['productData']['unique'] = $searchperienceHelper->getProductUniqueId($productId, $storeId);
-        $returnData['productData']['rating'] = $product->getRatingSummary()->getRatingSummary();
+
+        if (Mage::getStoreConfigFlag('searperience/include_data/rating')) {
+            // fetch review data for requested product
+            Mage::getModel('review/review')->getEntitySummary($product, $storeId);
+            $returnData['productData']['rating'] = $product->getRatingSummary()->getRatingSummary();
+        }
 
         $this->_usedFields   = array_merge($this->_usedFields, array('id', 'description', 'short_description', 'price', 'name', 'tax_class_id'));
 
-        // fetch price information
-        $returnData = $this->_getProductPriceInformation($product, $returnData);
-
-        // fetch image information
-        $returnData = $this->_getProductImageInformation($product, $returnData);
-
-        $returnData = $this->fillProductCategoryInformation($product, $returnData);
-
-        // fetch related products
-        foreach ($this->getLinkedProductIds($productId) as $relatedProduct) {
-            $returnData['productData']['related'][] = $relatedProduct;
+        if (Mage::getStoreConfigFlag('searperience/include_data/prices')) {
+            // fetch price information
+            $returnData = $this->_getProductPriceInformation($product, $returnData);
         }
 
-        // fetch upsell products
-        foreach ($this->getLinkedProductIds($productId, Mage_Catalog_Model_Product_Link::LINK_TYPE_UPSELL) as $upsellProduct) {
-            $returnData['productData']['upsell'][] = $upsellProduct;
+        if (Mage::getStoreConfigFlag('searperience/include_data/images')) {
+            // fetch image information
+            $returnData = $this->_getProductImageInformation($product, $returnData);
         }
 
-        // fetch crosssell products
-        foreach ($this->getLinkedProductIds($productId, Mage_Catalog_Model_Product_Link::LINK_TYPE_CROSSSELL) as $crossProduct) {
-            $returnData['productData']['cross'][] = $crossProduct;
+        if (Mage::getStoreConfigFlag('searperience/include_data/categories')) {
+            $returnData = $this->fillProductCategoryInformation($product, $returnData);
+        }
+
+        if (Mage::getStoreConfigFlag('searperience/include_data/related')) {
+            // fetch related products
+            foreach ($this->getLinkedProductIds($productId) as $relatedProduct) {
+                $returnData['productData']['related'][] = $relatedProduct;
+            }
+        }
+
+        if (Mage::getStoreConfigFlag('searperience/include_data/upsell')) {
+            // fetch upsell products
+            foreach ($this->getLinkedProductIds($productId, Mage_Catalog_Model_Product_Link::LINK_TYPE_UPSELL) as $upsellProduct) {
+                $returnData['productData']['upsell'][] = $upsellProduct;
+            }
+        }
+
+        if (Mage::getStoreConfigFlag('searperience/include_data/cross')) {
+            // fetch crosssell products
+            foreach ($this->getLinkedProductIds($productId, Mage_Catalog_Model_Product_Link::LINK_TYPE_CROSSSELL) as $crossProduct) {
+                $returnData['productData']['cross'][] = $crossProduct;
+            }
         }
 
         foreach ($productIndexData as $attributeCode => $value) {
