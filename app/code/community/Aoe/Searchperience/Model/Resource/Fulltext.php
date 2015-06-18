b127fa31dcff6cc6fd9c814cb2c18b9f94ab8e61
Fabrizio Branca <fabrizio.branca@aoe.com>
2015-06-18 20:35:02 +0000

    Enabled sending documents

--- app/code/community/Aoe/Searchperience/Model/Resource/Fulltext.php
+++ app/code/community/Aoe/Searchperience/Model/Resource/Fulltext.php
@@ -82,14 +82,13 @@ class Aoe_Searchperience_Model_Resource_Fulltext {
                 if (empty($data['url'])) { Mage::throwException('No product url found'); }
                 if (empty($data['raw_document'])) { Mage::throwException('No raw_document found'); }
 
-                echo $data['raw_document'];
-
-//                $searchperienceApi->addDocument(
-//                    $data['raw_document'],
-//                    $productId . '_' . $storeId,
-//                    $data['url'],
-//                    $sourceIdentifier
-//                );
+                // echo $data['raw_document'];
+                $searchperienceApi->addDocument(
+                    $data['raw_document'],
+                    $productId . '_' . $storeId,
+                    $data['url'],
+                    $sourceIdentifier
+                );
             }
 
             // add new products to the processed ones
